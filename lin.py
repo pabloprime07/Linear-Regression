bd9c3efed7e30d78812e4f390e832b8cc68cac0f
pabloprime07 <pabloprime07@gmail.com>
2018-11-04 20:40:07 +0530

    Final

--- lin.py
+++ lin.py
@@ -1,28 +1,22 @@
 
 import matplotlib.pyplot as plt 
 import numpy as np 
-from sklearn import datasets, linear_model, metrics 
+from sklearn import linear_model, metrics 
 from sklearn.model_selection import train_test_split 
 import csv
 
 with open('lego_final.csv', 'r') as f:
     data = list(csv.reader(f, delimiter=','))
 data = np.array(data[1:], dtype=np.float)
-y1 = data[:,1]
+y = data[:,1]
 data = np.delete(data,1,1)
-print(data)
-print(y1)
-# load the boston dataset 
-boston = datasets.load_boston(return_X_y=False) 
+# print(data)
+# print(y1)
   
 # defining feature matrix(X) and response vector(y) 
-# X = boston.data 
-# y = boston.target 
-  
+
 X = data
-y = y1
 # print(X)
-print("\n")
 # print(y)
 # splitting X and y into training and testing sets 
 
@@ -49,11 +43,11 @@ print('Variance score: {}'.format(reg.score(X_test, y_test)))
 plt.style.use('fivethirtyeight') 
   
 ## plotting residual errors in training data 
-plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, 
+plt.scatter(y_train, y_train - reg.predict(X_train) , 
             color = "green", s = 10, label = 'Train data') 
   
 ## plotting residual errors in test data 
-plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test, 
+plt.scatter(y_test,  y_test - reg.predict(X_test) , 
             color = "blue", s = 10, label = 'Test data') 
   
 ## plotting line for zero residual error 
